029ba6d2e4c285d8b42f9c72b09d080ff1a4b22f
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2016-11-30 23:38:00 -0600

    Utility class for general functionality methods

---
+++ app/src/main/java/com/jaydot2/fitnessapp/basic/util/Util.java
@@ -0,0 +1,43 @@
+package com.jaydot2.fitnessapp.basic.util;
+
+/**
+ * <b>Description:</b>
+ * <p>
+ *     This class contains some helpful utility methods that are used across the application
+ * </p>
+ * Created by jamesbray on 11/30/16.
+ */
+
+public class Util {
+
+    private static final int CONVERSION_FACTOR = 703;
+
+
+    /**
+     * <p>
+     *     Raw BMI calculation - (pounds/inches^2 * 703)
+     * </p>
+     * @param mass
+     * @param height
+     * @return
+     */
+    public double calculateStandardBMI(int mass, int height) {
+        double bmi = 0.0;
+        bmi = mass / height * CONVERSION_FACTOR;
+        return bmi;
+    }
+
+    /**
+     * <p>
+     *     Raw BMI calculation - metric (kilograms/meters^2)
+     * </p>
+     * @param mass
+     * @param height
+     * @return
+     */
+    public double calculateMetricBMI(int mass, int height) {
+        double bmi = 0.0;
+        bmi = mass / height;
+        return bmi;
+    }
+}
