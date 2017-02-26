507385e5347d9b768c17f8d8d5a64f50ee984962
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2017-02-26 15:27:49 -0600

    Calorie calculation functionality

---
+++ app/src/main/java/com/jaydot2/fitnessapp/basic/util/FitnessCalculator.java
@@ -0,0 +1,62 @@
+package com.jaydot2.fitnessapp.basic.util;
+
+/**
+ * Created by jamesbray on 2/26/17.
+ */
+
+public class FitnessCalculator {
+
+    private FitnessCalculator fitnessCalc;
+
+    protected FitnessCalculator() {
+        //
+    }
+
+
+    public FitnessCalculator getInstance() {
+        if(this.fitnessCalc == null) {
+            this.fitnessCalc = new FitnessCalculator();
+        }
+        return this.fitnessCalc;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double runningCalarieCalc() {
+        double result = 0.0;
+
+        return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double cyclingCalarieCalc() {
+        double result = 0.0;
+
+        return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double walkingCalarieCalc() {
+        double result = 0.0;
+
+        return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double swimmingCalarieCalc() {
+        double result = 0.0;
+
+        return result;
+    }
+}
