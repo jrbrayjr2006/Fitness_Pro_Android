6ef282d158eda19c754005e7fd74b8df81266c1c
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2017-02-26 15:26:55 -0600

    Junit test for the calarie calculation functionality

---
+++ app/src/test/java/com/jaydot2/fitnessapp/basic/util/FitnessCalculatorTest.java
@@ -0,0 +1,55 @@
+package com.jaydot2.fitnessapp.basic.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by jamesbray on 2/26/17.
+ */
+public class FitnessCalculatorTest {
+
+    @Before
+    public void setUp() {
+        //
+    }
+
+    @After
+    public void tearDown() {
+        //
+    }
+
+    /**
+     *
+     */
+    public void runningCalarieCalcTest() {
+        double result = 0.0;
+        fail("not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public void cyclingCalarieCalcTest() {
+        double result = 0.0;
+        fail("not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public void walkingCalarieCalcTest() {
+        double result = 0.0;
+        fail("not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public void swimmingCalarieCalcTest() {
+        double result = 0.0;
+        fail("not implemented yet!");
+    }
+}
