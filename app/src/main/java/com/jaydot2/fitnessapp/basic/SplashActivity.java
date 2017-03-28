fd179fa9d8d0d78137179d2da5c7fa41cdd9de4e
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2017-03-27 21:07:09 -0500

    Added splash screen to app.

---
+++ app/src/main/java/com/jaydot2/fitnessapp/basic/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.jaydot2.fitnessapp.basic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <p>
+ *     This is for the splash screen when the app opens
+ * </p>
+ * Created by jamesbray on 3/27/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    public SplashActivity() {
+        super();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
